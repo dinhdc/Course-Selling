7c019fb3f26e9ad15d8479df2db6ac07b37fc2b6
dinh <congidnh.work@gmail.com>
2023-05-03 11:10:09 +0700

    welcome screen

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ulearning_app/app_blocs.dart';
+import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
+import 'package:ulearning_app/pages/welcome/welcome.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => WelcomeBloc()),
+          BlocProvider(create: (context) => AppBlocs()),
+        ],
+        child: ScreenUtilInit(
+            builder: (context, child) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: const Welcome(),
+              routes: {
+                "myHomePage": (context) => const MyHomePage()
+              },
+            )));
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Text("no thing in here");
+  }
+}
